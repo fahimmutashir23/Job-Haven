4e02b819e311e481f5f71b7c09e635822b342d72
fahimmutashir23 <taslimit23@gmail.com>
2023-11-05 15:09:13 +0600

    create all jobs section

--- src/Pages/AllJobs/AllJobs.jsx
+++ src/Pages/AllJobs/AllJobs.jsx
@@ -1,10 +1,54 @@
+import { useEffect, useState } from "react";
+import useAxios from "../../Hooks/useAxios";
+import CategoryJob from "../Home/CategoryJob";
 
 const AllJobs = () => {
-    return (
-        <div>
-            all jobs            
+  const [data, setData] = useState(null);
+  const axios = useAxios();
+
+  useEffect(() => {
+    axios.get(`/jobs`).then((res) => setData(res.data));
+  }, [axios]);
+
+  return (
+    <div>
+      <div className="flex justify-between items-center mt-4 bg-gradient-to-r from-base-200 px-2 rounded-md">
+        <h1 className=" text-3xl font-bold">All Jobs Here</h1>
+
+        <div className="form-control">
+          <div className="input-group">
+            <input
+              type="text"
+              placeholder="Search…"
+              className="input input-bordered"
+            />
+            <button className="btn btn-square">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
-    );
+      </div>
+
+      <div className="md:grid grid-cols-2 gap-3 my-5">
+        {data?.map((item) => (
+          <CategoryJob key={item._id} job={item}></CategoryJob>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AllJobs;
--- src/Pages/Home/CategoryJob.jsx
+++ src/Pages/Home/CategoryJob.jsx
@@ -8,7 +8,7 @@ import { BsPersonCircle, } from "react-icons/Bs";
 
 const CategoryJob = ({ job }) => {
   const { user } = useContext(AuthContext);
-  const { job_title, posting_date, deadline, salary_range, apply_number } = job;
+  const { _id, job_title, posting_date, deadline, salary_range, apply_number } = job;
 
   return (
     <div>
@@ -25,7 +25,7 @@ const CategoryJob = ({ job }) => {
         </div>
         <div className="flex justify-end">
           <Link
-            to=""
+            to={`/jobs/${_id}`}
             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Read more
