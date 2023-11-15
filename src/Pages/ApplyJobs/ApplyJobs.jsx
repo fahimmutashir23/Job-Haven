4983bd135a46eba6dbc8596c676a644d2a417c84
fahimmutashir23 <taslimit23@gmail.com>
2023-11-15 14:46:46 +0600

    fix filter bugs

--- src/Pages/ApplyJobs/ApplyJobs.jsx
+++ src/Pages/ApplyJobs/ApplyJobs.jsx
@@ -4,49 +4,51 @@ import { AuthContext } from "../../Provider/Provider";
 import ApplyCard from "./ApplyCard";
 import { Helmet } from "react-helmet-async";
 
-
 const ApplyJobs = () => {
   const axios = useAxios();
   const { user } = useContext(AuthContext);
   const [data, setData] = useState(null);
-
+  const [renderData, setRenderData] = useState(null);
+  
   useEffect(() => {
-    axios.get(`applyJob?email=${user.email}`).then((res) => {
+    axios.get(`/applyJob?email=${user.email}`).then((res) => {
       setData(res.data);
+      setRenderData(res.data);
     });
   }, [axios, user]);
 
   const handleFilter = (e) => {
-    const filter = e.target.value
-    const filterData = data?.filter(item => item.category === filter)
-    setData(filterData)
-  }
+    const filter = e.target.value;
+    const filterData = data?.filter((item) => item.category === filter);
+    setRenderData(filterData);
+  };
 
   return (
     <div>
-        <Helmet><title>Job Heaven | Apply Jobs</title></Helmet>
+      <Helmet>
+        <title>Job Heaven | Apply Jobs</title>
+      </Helmet>
       <h1 className="text-center font-bold text-3xl bg-base-200 py-2 px-8 rounded-b-lg max-w-fit mx-auto mb-5">
         Your Apply
       </h1>
       <div className="mb-6">
-                <label className="block mb-2 text-sm font-medium">
-                  Job Category
-                </label>
-                <select 
-                name="category"
-                onChange={handleFilter} 
-                className=" text-sm rounded-lg block w-52 p-2.5 dark:placeholder-gray-400 dark:shadow-sm-light"
-                  required>
-                    <option value="Part Time">Part Time</option>
-                    <option value="Hybrid">Hybrid</option>
-                    <option value="On Site Job">On Site Job</option>
-                    <option value="Remote Job">Remote Job</option>
-                  </select>
-              </div>
+        <label className="block mb-2 text-sm font-medium">Job Category</label>
+        <select
+          name="category"
+          onChange={handleFilter}
+          className=" text-sm rounded-lg block w-52 p-2.5 dark:placeholder-gray-400 dark:shadow-sm-light"
+          required
+        >
+          <option value="Part Time">Part Time</option>
+          <option value="Hybrid">Hybrid</option>
+          <option value="On Site Job">On Site Job</option>
+          <option value="Remote Job">Remote Job</option>
+        </select>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5 my-5">
-       {
-        data?.map(item => <ApplyCard key={item._id} job={item}></ApplyCard>)
-       }
+        {renderData?.map((item) => (
+          <ApplyCard key={item._id} job={item}></ApplyCard>
+        ))}
       </div>
     </div>
   );
